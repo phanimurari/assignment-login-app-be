ae40c53a75cf69235c5956ae472bc6429cd35101
aidoitdev <aidoit.dev@gmail.com>
2025-01-09 18:38:44 +0530

    implemented-cors-error

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const authRoutes = require('./routes/authRoute');
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
